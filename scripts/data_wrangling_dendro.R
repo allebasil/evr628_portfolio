a1973036aeea0716897439a73bca181f30f19003
allebasil <childress.isabella@gmail.com>
2025-11-24 17:43:51 -0500

    Practice data control

--- scripts/data_wrangling_dendro.R
+++ scripts/data_wrangling_dendro.R
@@ -29,7 +29,18 @@ all_data <- readr::read_csv(dendro_csvs,
                             col_names = FALSE,
                             show_col_types = FALSE)
 
-## Check this actually happened
+### Optional: Check this actually happened
 str(all_data[, 1:10])
 
-##
+## Clean the data (rearrange and title columns, etc.)
+### This is the actual cleaning!!!!! Note to self: Here is where you're doing it!
+### I plan to create an object called "clean_data" and that will be the cleaned
+### data ready to export
+### clean_data <- all_data |>
+  # e.g. rename columns, filter bad rows, parse dates, etc.
+  # dplyr::rename(...)
+  # dplyr::filter(...)
+  # dplyr::mutate(...)
+
+## Export cleaned version of data
+readr::write_csv(all_data, here("data", "Dendrometer_Data_clean.csv"))
